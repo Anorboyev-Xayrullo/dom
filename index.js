6b067ff59144b065e8010a7d4130d130a634b3b1
Anorboyev-Xayrullo <anorboyevxayrullo841@gmail.com>
2023-05-01 15:56:11 +0500

    main

---
+++ index.js
@@ -0,0 +1,21 @@
+let count = document.querySelector(".count")
+let reset = document.querySelector(".reset")
+let num = 0
+count.onclick = () =>{
+    num += 1
+    count.innerHTML = num
+    if (count.innerHTML == 5){
+      count.innerHTML = 0;
+      num=0;
+    }else{
+        console.log("xato");
+    }
+   
+}
+
+reset.onclick = () =>{
+    num = 0 
+    count.innerHTML = num
+}
+
+
